08c1ac4190b2d9498b9a30c9db7709068b26134d
Tunem <70690346+Tunem@users.noreply.github.com>
2022-10-25 13:49:49 +0300

    fixed material writing

--- Source/EscapeInSpace/Robot.h
+++ Source/EscapeInSpace/Robot.h
@@ -24,7 +24,8 @@ public:
   
   UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Robot setings")
   UMaterialInterface *SeppoMaterial;
-    
+  
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Robot setings")
   UMaterialInstanceDynamic *RobotMaterialInstance;
 
 protected:
